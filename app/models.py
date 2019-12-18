8f7e51b05657d144e59e19462ec92b1c772da28b
Kris Leland <kristopherleland@gmail.com>
2019-12-17 17:07:55 -0700

    user model done

--- app/models.py
+++ app/models.py
@@ -1,10 +1,16 @@
 from app import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    email = db.Column(db.String(64), index=True, unique=True)
+    elo = db.Column(db.String(120), index=True, unique=True)
+    wins = db.Column(db.Integer)
+    losses = db.Column(db.Integer)
+    date_added = db.Column(db.DATETIME, timezone=False)
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
