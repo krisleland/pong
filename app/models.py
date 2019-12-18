e066edf0dbcc33c3592765d2d5baa00e49a178db
Kris Leland <kristopherleland@gmail.com>
2019-12-17 17:40:04 -0700

    more db changes

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), index=True, unique=True)
-    elo = db.Column(db.String(120), index=True, unique=True)
+    elo = db.Column(db.Float, index=True, unique=True)
     wins = db.Column(db.Integer)
     losses = db.Column(db.Integer)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
@@ -13,5 +13,16 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    winner_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    loser_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    resolved_challenge_id = db.Column(db.Integer)
+    season = db.Column(db.Integer)
+    winner = db.relationship('User', foreign_keys='Match.winner_id')
+    loser = db.relationship('User', foreign_keys='Match.loser_id')
 
 
