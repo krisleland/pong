a10c62cec65010b4386570364bc98c8d10985b9d
Kris Leland <kristopherleland@gmail.com>
2019-12-29 15:03:27 -0500

    added requirements file

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
